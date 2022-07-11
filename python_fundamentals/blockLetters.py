19a7b4097e09c62fcc9ee17f3eda03fcd2a6d0c0
Charles Ngo <charlesngo37@gmail.com>
2022-07-11 15:04:06 -0700

    initial project to print initials to console

---
+++ python_fundamentals/blockLetters.py
@@ -0,0 +1,17 @@
+# Project to print letters as 5*7 blocks
+
+print(" CCC ")
+print("C   C")
+print("C    ")
+print("C    ")
+print("C    ")
+print("C   C")
+print(" CCC ")
+
+print("N   N")
+print("NN  N")
+print("N N N")
+print("N  NN")
+print("N   N")
+print("N   N")
+print("N   N")
