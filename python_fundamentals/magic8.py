aa678d67000f703eaf2e4001d8cdfe74873194ab
Charles Ngo <charlesngo37@gmail.com>
2022-07-13 11:59:00 -0700

    add random number generator

--- python_fundamentals/magic8.py
+++ python_fundamentals/magic8.py
@@ -1,2 +1,8 @@
+# Function will take in a name and question and print out a magic eight ball style response
+import random
+
+# Variables for the magic eightball to print to console
 name = "charles"
 question = "Will dinner be fried chicken?"
+answer = ""
+random_number = random.randint(1, 9)
