6ce12e90dab28902c45204b5367d8b34b90a6bcf
Charles Ngo <charlesngo37@gmail.com>
2022-07-14 15:03:17 -0700

    initial commit for gradebook

---
+++ python_fundamentals/gradebook.py
@@ -0,0 +1,15 @@
+last_semester_gradebook = [
+    ["politics", 80],
+    ["latin", 96],
+    ["dance", 97],
+    ["architecture", 65],
+]
+
+# Your code below:
+
+subjects = ["physics", "calculus", "poetry", "history"]
+grades = [80, 96, 97, 65]
+
+gradebook = [["physics", 80], ["calculus", 97], ["poetry", 85], ["history", 88]]
+
+print(gradebook)
