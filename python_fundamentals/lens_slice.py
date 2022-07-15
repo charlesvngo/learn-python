f7658aab265f39640c2a51a655d7cbe506c31049
Charles Ngo <charlesngo37@gmail.com>
2022-07-15 15:39:41 -0700

    initial commit for lens_pizza.py

---
+++ python_fundamentals/lens_slice.py
@@ -0,0 +1,28 @@
+# Make some pizzas!
+toppings = [
+    "pepperoni",
+    "pineapple",
+    "cheese",
+    "sausage",
+    "olives",
+    "anchovies",
+    "mushrooms",
+]
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = prices.count(2)
+num_pizzas = len(toppings)
+
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices = [
+    [2, "pepperoni"],
+    [6, "pineapple"],
+    [1, "cheese"],
+    [3, "sausage"],
+    [2, "olives"],
+    [7, "anchovies"],
+    [2, "mushrooms"],
+]
+
+print(pizza_and_prices)
