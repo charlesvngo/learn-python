4a30d8345bfc0b53fcaa98e08d0285c1f09a36e3
Charles Ngo <charlesngo37@gmail.com>
2022-07-27 12:52:20 -0700

    solve temperature problems

---
+++ python_fundamentals/physics_class.py
@@ -0,0 +1,30 @@
+# Uncomment this when you reach the "Use the Force" section
+# train_mass = 22680
+# train_acceleration = 10
+# train_distance = 100
+# bomb_mass = 1
+
+
+# Write your code below:
+
+# Turn up the temperature
+# Temp (C) = (Temp (F) - 32) * 5/9
+# Temp (F) = Temp (C) * (9/5) + 32
+
+
+def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * 5 / 9
+    return c_temp
+
+
+f100_in_celsius = f_to_c(100)
+print(f100_in_celsius)
+
+
+def c_to_f(c_temp):
+    f_temp = (c_temp * 9 / 5) + 32
+    return f_temp
+
+
+c0_in_fahrenheit = c_to_f(0)
+print(c0_in_fahrenheit)
