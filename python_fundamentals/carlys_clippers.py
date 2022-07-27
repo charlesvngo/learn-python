5e9eb33fd3aa9f3e750bbff0f5db1146fbc8efbb
Charles Ngo <charlesngo37@gmail.com>
2022-07-26 21:39:16 -0700

    solve prices & cut problems

---
+++ python_fundamentals/carlys_clippers.py
@@ -0,0 +1,29 @@
+# Initial Values
+hairstyles = [
+    "bouffant",
+    "pixie",
+    "dreadlocks",
+    "crew",
+    "bowl",
+    "bob",
+    "mohawk",
+    "flattop",
+]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+
+# Prices and cuts
+total_price = 0
+
+for price in prices:
+    total_price += price
+
+average_price = total_price / len(prices)
+
+print(total_price)
+print(average_price)
+
+new_prices = [price - 5 for price in prices]
+print(new_prices)
