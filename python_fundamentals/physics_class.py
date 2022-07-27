daed54f720c8427e710257519db1794cb414bb2c
Charles Ngo <charlesngo37@gmail.com>
2022-07-27 13:18:31 -0700

    solve force equation

--- python_fundamentals/physics_class.py
+++ python_fundamentals/physics_class.py
@@ -1,10 +1,3 @@
-# Uncomment this when you reach the "Use the Force" section
-# train_mass = 22680
-# train_acceleration = 10
-# train_distance = 100
-# bomb_mass = 1
-
-
 # Write your code below:
 
 # Turn up the temperature
@@ -28,3 +21,27 @@ def c_to_f(c_temp):
 
 c0_in_fahrenheit = c_to_f(0)
 print(c0_in_fahrenheit)
+
+# Use the force
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+
+def get_force(mass, acceleration):
+    force = mass * acceleration
+    return force
+
+
+train_force = get_force(train_mass, train_acceleration)
+print("The GE train supplies " + str(train_force) + " Newtons of force.")
+
+
+def get_energy(mass, c=3 * 10**8):
+    energy = mass * c
+    return energy
+
+
+bomb_energy = get_energy(bomb_mass)
+print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
