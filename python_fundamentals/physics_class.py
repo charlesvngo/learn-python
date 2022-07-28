171f610bb4c034c44e998958da3be1688de8276e
Charles Ngo <charlesngo37@gmail.com>
2022-07-27 19:21:43 -0700

    solve work equations

--- python_fundamentals/physics_class.py
+++ python_fundamentals/physics_class.py
@@ -45,3 +45,19 @@ def get_energy(mass, c=3 * 10**8):
 
 bomb_energy = get_energy(bomb_mass)
 print("A 1kg bomb supplies " + str(bomb_energy) + " Joules")
+
+# Do the work
+def get_work(mass, acceleration, distance):
+    force = get_force(mass, acceleration)
+    work = force * distance
+    return work
+
+
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print(
+    "The GE train does "
+    + str(train_work)
+    + " Joules of work over "
+    + str(train_distance)
+    + " meters."
+)
